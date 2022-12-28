57762cdb1ecdb56bbead841e7a86c9a2ebed3f0c
andiarwidiar1 <andiarkurnia1234@gmail.com>
2022-12-28 14:55:08 +0700

    up

--- app/src/main/java/com/example/rismajt/UserProfile.java
+++ app/src/main/java/com/example/rismajt/UserProfile.java
@@ -3,6 +3,9 @@ package com.example.rismajt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class UserProfile extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class UserProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
